ae193f5495e3deba7d7f445281247634f112ba61
Lohith10039 <lohith6302@gmail.com>
2025-03-03 09:48:13 +0530

    Add files via upload

---
+++ ex_2.java
@@ -0,0 +1,8 @@
+class ex_2 {
+    public static void main(String args[]) {
+        System.out.println("Name: lohith");
+        System.out.println("Class: CSE A");
+        System.out.println("Roll No: 24039");
+      
+    }
+}
