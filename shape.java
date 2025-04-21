187cf1f130b75f8d4bbc9ea6b46ae3a47394c30c
Lohith10039 <lohith6302@gmail.com>
2025-04-21 12:07:21 +0530

    Add files via upload

--- shape.java
+++ shape.java
@@ -1,25 +1,58 @@
+interface shape{
+	
+	int getPerimeter();
 
-
-class shape{
-    public Double area( Double a ){
-        return a*a;
-    }
-    public  Double area(Double length , Double breadth ){
-        return length*breadth;
-    }
 }
-class circle extends shape{
-    public Double area(Double radius){
-        return 3.14*radius*radius;
+class traingle implements shape{
+	int side1,side2,side3;
+	triangle(int side1,int side2,int side3){
+		this.side1=side1;
+		this.side2=side2;
+		this.side3=side3;
+	
+}
+ 	int getPerimeter(){
+		return side1+side2+side3;
+
+}
+class rectangle implements shape{
+	int length; int breadth;
+	rectangle(int length,int breadth){
+		this.length=length;
+		this.breadth=breadth;
+
+}
+int getPerimeter(){
+		return 2*(length+breadth);
+
+}
+}
+class Circle implements Shape {
+   int radius;
+    private static final double PI = 3.14159;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * PI * radius;
+
+
     }
 }
 
 class main{
-    public static void main(String[]args){
-        shape s = new shape();
-        circle c = new circle();
-        System.out.println("area of square is "+s.area(5.0));
-        System.out.println("area of rectangle is"+s.area(5.0,6.0));
-        System.out.println("area of circle is "+c.area(3.0));
+    public static void main(String[] args) {
+
+        rectangle rec = new rectangle(5, 3);
+        Circle circle = new Circle(4);
+        triangle tri = new triangle(3, 4, 5);
+
+        System.out.println("Rectangle Perimeter: " + rec.getPerimeter());
+        System.out.println("Circle Perimeter: " + circle.getPerimeter());
+        System.out.println("Triangle Perimeter: " + tri.getPerimeter());
     }
+
+}
 }
